442110974d3372637304e97300403bfb77542eca
nitesh <gaddalaynitesh08@gmail.com>
2021-09-18 19:36:23 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+function fn1()
+{
+    var lname = document.getElementById('lname').value;
+    var fname = document.getElementById('fname').value;
+    var count = document.getElementById('country').value;
+    var numb = document.getElementById('phone').value;
+    var phot = document.getElementById('image').value;
+
+    document.getElementById("f-value").innerHTML = fname;
+    document.getElementById("l-value").innerHTML = lname;
+    document.getElementById("nation").innerHTML = count;
+    document.getElementById("phoned").innerHTML = numb;
+}
+
+
+
